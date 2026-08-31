371d5010e60c4fe72e87f899bcb40fc71991cf14
RuthwikSai18 <ruthwikcybersecuritynri@gmail.com>
2026-08-31 15:58:50 +0530

    Comparision_Operators_Exercise_Solutions

---
+++ Comparision_ Operators_Exercise.py
@@ -0,0 +1,15 @@
+#Comparision operators exercise
+a=3
+b=6
+result= a==b #Equal to comparision operator
+print("result of",a, "==",b,"is : ", result)
+result= a!=b #Not equal to comparision operator
+print("result of",a, "!=",b,"is : ", result)
+result= a >b #Greater than comparision operator
+print("result of",a, ">",b,"is : ", result)
+result= a<b #Less than comparision operator
+print("result of",a, "<",b,"is : ", result)
+result= a>=b #Greater than or equal to
+print("result of",a, ">=",b,"is : ", result)
+result= a<=b #Less than or equal to
+print("result of",a, "<=",b,"is : ", result)
