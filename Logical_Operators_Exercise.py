77903d8c14396b2d9726e3d1f591623cc9f601cd
RuthwikSai18 <ruthwikcybersecuritynri@gmail.com>
2026-09-01 14:11:20 +0530

    Logical Operators Exercise

---
+++ Logical_Operators_Exercise.py
@@ -0,0 +1,22 @@
+#Logiacl Operators Exercise
+#AND Operator
+a = 10
+result = a > 15 and a < 5
+print("result of", a, "> 15 and", a, "<5: ", result)
+a = 15
+result = a > 10 and a < 20
+print("result of", a, "> 10 and", a, "<20: ", result)
+#OR Operator
+a=5
+result = a > 2 and a < 10
+print("result of", a, "> 2 or", a, "<10 :", result)
+a=8
+result = a > 10 and a < 20
+print("result of", a, ">10 or", a, "<20 :", result)
+#NOT Operator
+a=20
+result = a > 15 and a < 15
+print("result of", a, ">15 not", a, "<15 :", result)
+a=50
+result = a > 20 and a <60
+print("result of", a, ">15 not", a, "<15 :", result)
