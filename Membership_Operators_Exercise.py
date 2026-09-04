545e968809d70a4c3a4fcb819249901af3563896
RuthwikSai18 <ruthwikcybersecuritynri@gmail.com>
2026-09-04 14:21:57 +0530

    Membership Operators

---
+++ Membership_Operators_Exercise.py
@@ -0,0 +1,13 @@
+#Membership Operators Exercise
+#in operator
+a = [1000,2000,3000,4000]
+result = 3000 in a
+print("result of"  ,3000, "in" ,a, "is :", result)
+result = 50 in a
+print("result of"  ,50, "in" ,a, "is :", result)
+#Not in operator
+a = [10,20,30,40]
+result = 50 not in a
+print("result of" ,50, "not in" ,a, "is :", result)
+result = 20 not in a
+print("result of" ,20, "not in" ,a, "is :", result)
