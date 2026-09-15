fcf160ebf959e473b40b3844ae6cb4d300c9bfd7
RUTHWIK SAI <ruthwikcybersecuritynri@gmail.com>
2026-09-15 13:34:06 +0530

    Simple Interest

---
+++ Simple Interest.py
@@ -0,0 +1,6 @@
+#Calculating SIMPLE INTEREST
+principle=float(input("Enter the principle amount:"))
+rate=float(input("Enter the rate of interest:"))
+time=float(input("Enter time in years:"))
+SI=(principle*rate*time)/100
+print(SI)
