4fa2df13be8ea181b303bfc6c26ab67970f18265
RUTHWIK SAI <ruthwikcybersecuritynri@gmail.com>
2026-09-15 13:43:39 +0530

    Operator precedence

---
+++ Operators_Precedence_Exercise.py
@@ -0,0 +1,35 @@
+#Operatots Precedence Exercises
+#Exercise-4
+a=10
+b=3
+print(f"The rasult of {a}/{b} is {a/b}")
+print(f"The result of {a}//{b} is {a//b}")
+#Exercise-5
+a=18
+b=3
+result=a*b//2
+print(f"The result of {a}*{b}//2 is {result}")
+#Exercise-6
+a=100
+result=a//10//2
+print(f"The result of {a}//10//2 is {result}")
+#Exercise-7
+a=2
+b=3
+result=a+b*2**2
+print(f"The result of {a}+{b}*2**2 is {result}")
+#Exercise-8
+a=5
+b=3
+result=(a+b)*2
+print(f"The result of ({a}+{b})*2 is {result}")
+#Exercise-9
+a=5
+b=3
+result=a+b*2>10
+print(f"The result of {a}+{b}*2>10 is {result}")
+#Exercise-10
+a=5
+b=3
+result=a>b & b>1 or a<0
+print(f"The result of {a}>{b} & {a}>{b} & {b}>1 or {a}<0 is {result}")
