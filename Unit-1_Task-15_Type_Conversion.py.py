3dfcfeaa54494304146400c52336ffaebc725bfd
RUTHWIK SAI <ruthwikcybersecuritynri@gmail.com>
2026-09-25 14:53:15 +0530

    Add files via upload

---
+++ Unit-1_Task-15_Type_Conversion.py.py
@@ -0,0 +1,30 @@
+#Float to integer type conversion
+float_num=69.9
+float_num=int(float_num)
+print(float_num)
+print(type(float_num))
+#Integer to string type conversions
+int_num=50
+int_num=float(int_num)
+print(int_num)
+print(type(int_num))
+#Integer to string type conversions
+int_num=20
+str_num=str(int_num)
+print(str_num)
+print(type(str_num))
+#Float to string type coversions
+float_num=50.5
+str_num=str(float_num)
+print(str_num)
+print(type(str_num))
+#String to integer type conversions
+str_num="20" 
+int_num=str(int_num)
+print(int_num)
+print(type(int_num))
+#String to float type conversions
+str_num="15.5"
+float_num=float(str_num)
+print(float_num)
+print(type(float_num))
