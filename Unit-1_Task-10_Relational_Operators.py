c5bb3dddf255088cf9c2e5cdcabea3f813a618a1
RUTHWIK SAI <ruthwikcybersecuritynri@gmail.com>
2026-09-25 14:46:44 +0530

    Add files via upload

---
+++ Unit-1_Task-10_Relational_Operators.py
@@ -0,0 +1,15 @@
+#Realtion Operators Exercise
+a=10
+b=20
+result = a > b 
+print(f"result of {a} > {b} is : " , result)
+result = a < b
+print(f"result of {a} < {b} is : " , result)
+result = a == b
+print(f"result of {a} == {b} is : " , result)
+result = a != b
+print(f"result of {a} != {b} is : " , result)
+result = a >= b
+print(f"result of {a} >= {b} is : " , result)
+result = a <= b
+print(f"result of {a} <= {b} is : " , result)
